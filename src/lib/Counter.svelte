c6396c63f47e289abd571779669995670e4d3059
MarisaCodes <marisakirisame955@gmail.com>
2023-11-17 21:28:31 +0100

    added tailwindcss

--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
-  let count: number = 0
+  let count: number = 0;
   const increment = () => {
-    count += 1
-  }
+    count += 1;
+  };
 </script>
 
-<button on:click={increment}>
+<button class="bg-emerald-400 rounded-lg p-3 px-4" on:click={increment}>
   count is {count}
 </button>
